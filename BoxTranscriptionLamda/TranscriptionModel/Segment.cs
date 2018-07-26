4d95fdaed1f413193ac6a9e3786ee94438d49320
Joshua Page <josh.page@robotsandpencils.com>
2018-07-26 11:54:20 -0400

    refactoring everything in a new project. Metadata based on json template

---
+++ BoxTranscriptionLamda/TranscriptionModel/Segment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWSTranscriptionLamda
+{
+
+    public class SegmentItem
+    {
+        public decimal start_time { get; set; }
+        public string speaker_label { get; set; }
+        public decimal end_time { get; set; }
+    }
+    public class Segment
+    {
+        public decimal start_time { get; set; }
+        public string speaker_label { get; set; }
+        public decimal end_time { get; set; }
+        public SegmentItem[] items { get; set; }
+    }
+}
---
+++ BoxTranscriptionLamda/TranscriptionModel/TranscribeItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWSTranscriptionLamda
+{
+    public class TranscribeItem
+    {
+        public decimal start_time { get; set; }
+        public string type { get; set; }
+        public decimal end_time { get; set; }
+        public List<TranscribeAlternative> alternatives { get; set; }
+    }
+
+    public class TranscribeAlternative
+    {
+        public double confidence { get; set; }
+        public string content { get; set; }
+    }
+}
